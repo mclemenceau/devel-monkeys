dd9df15e3ea71a3ad111c9a0abf1190bb3914bcf
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2025-03-14 18:19:20 -0500

    Added build color enhancer

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ I've used this mostly while contributing to Ubuntu but I don't see why it can't 
 
 Currently, this repository includes:
 - ![Color Diff Icon](icons/color-diff-icon-16x16.png) **[Color Diff Viewer (Tampermonkey Script)](https://raw.githubusercontent.com/mclemenceau/devel-monkeys/main/scripts/color-diff.user.js)**: Enhances `.diff`, `.diff.gz` and `.patch` files viewed in Chrome/Firefox by adding syntax highlighting (green for additions, red for deletions, orange for headers).
+**[Color Build Log Viewer (Tampermonkey Script)](https://raw.githubusercontent.com/mclemenceau/devel-monkeys/main/scripts/color-buildlog.user.js)**: Enhances `BUILDING.txt.gz` log files viewed in Chrome/Firefox by adding syntax highlighting for warnings and Error. Use mostly for Ubuntu packages build failures.
 
 This project is intended to grow with more scripts relevant to helping development operations.
 
---
+++ scripts/color-buildlog.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Color build logs
+// @namespace    https://github.com/mclemenceau
+// @version      1.0.1
+// @author       Matthieu Clemenceau
+// @description  Highlights Build log Error and Warnings
+// @homepage     https://github.com/mclemenceau/devel-monkeys
+// @homepageURL  https://github.com/mclemenceau/devel-monkeys
+// @updateURL    https://raw.githubusercontent.com/mclemenceau/devel-monkeys/main/scripts/color-buildlog.user.js
+// @downloadURL  https://raw.githubusercontent.com/mclemenceau/devel-monkeys/main/scripts/color-buildlog.user.js
+// @supportURL   https://github.com/mclemenceau/devel-monkeys/issues
+// @match        *://*/*BUILDING.txt.gz
+
+(function() {
+    'use strict';
+
+    function highlightLog() {
+        const pre = document.querySelector("pre"); // Build logs are usually inside a <pre> tag
+        if (!pre)
+            return;
+
+        let logText = pre.innerHTML;
+
+        // Apply color coding to different log messages
+        const replacements = [
+            { regex: /(^|\s)(error:.*?)(\s|$)/gi, color: 'red' }, // Errors
+            { regex: /(^|\s)(warning:.*?)(\s|$)/gi, color: 'orange' }, // Warnings
+            { regex: /^E: .*/gm, color: 'red', bold: true } // Lines starting with "E: "
+        ];
+
+        // Wrap detected keywords with colored spans
+        replacements.forEach(({ regex, color, bold }) => {
+            logText = logText.replace(regex, (match) => {
+                return `<span style="color:${color}; ${bold ? 'font-weight:bold;' : ''}">${match}</span>`;
+            });
+        });
+
+        // Replace the preformatted text with the new styled version
+        pre.innerHTML = logText;
+    }
+
+    highlightLog();
+
+})();
+
+// ==/UserScript==
